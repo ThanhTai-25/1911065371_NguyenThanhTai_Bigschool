6dda0a3a286be21aab892c9948e780e7b22e638e
MSII <MSII@DESKTOP-1F79LGU>
2022-03-21 11:48:36 +0700

    Add project files.

---
+++ 1911065371_NguyenThanhTai_Bigschool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1911065371_NguyenThanhTai_Bigschool.Startup))]
+namespace _1911065371_NguyenThanhTai_Bigschool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
