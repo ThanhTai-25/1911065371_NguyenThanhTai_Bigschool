fc04f4728d886b4b754b1b1fbf31f92767fa0d8f
MSII <MSII@DESKTOP-1F79LGU>
2022-03-23 16:46:52 +0700

    Commit 23/3/2022

--- 1911065371_NguyenThanhTai_Bigschool/App_Start/BundleConfig.cs
+++ 1911065371_NguyenThanhTai_Bigschool/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace _1911065371_NguyenThanhTai_Bigschool
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
