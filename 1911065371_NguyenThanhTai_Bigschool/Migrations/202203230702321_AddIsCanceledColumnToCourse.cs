cb4e024cd9ebbf603be5bbabcb446ce68d5233fb
MSII <MSII@DESKTOP-1F79LGU>
2022-03-23 14:26:40 +0700

    Commit 23/3/2022

---
+++ 1911065371_NguyenThanhTai_Bigschool/Migrations/202203230702321_AddIsCanceledColumnToCourse.cs
@@ -0,0 +1,16 @@
+﻿namespace _1911065371_NguyenThanhTai_Bigschool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsCanceledColumnToCourse : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ 1911065371_NguyenThanhTai_Bigschool/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using _1911065371_NguyenThanhTai_Bigschool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1911065371_NguyenThanhTai_Bigschool.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+
+        public bool ShowAction { get; set; }
+    }
+}
